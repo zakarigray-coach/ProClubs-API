3e0812ee9919ba0bd5bf2c9002316d151a946702
zakarigray-coach <zakarigray@gmail.com>
2026-09-21 10:34:35 -0400

    Use RT Media newspaper template for article graphics

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const { buildResponse, archetypes, costModels } = require('./optimizer');
+require('./rtNewspaperPatch');
 const { startBot } = require('./reporterBot');
 
 const app = express();
